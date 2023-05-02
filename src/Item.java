b79de36cc16bfeaa1d78bdde6423bee4b284feda
BT_4N2_01 <mchoudhary4645@bths.edu>
2023-05-02 12:18:36 -0400

    ITEM ADDITION

--- src/Item.java
+++ src/Item.java
@@ -1,11 +1,20 @@
 public class Item {
     private String name;
     private double price;
+    private String description;
+
+    private String itemType;
 
 
     public Item(double price, String name){
         this.name = name;
         this.price = price;
+    }
+    public Item(double price,String name, String description, String ItemType){
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.itemType = ItemType;
     }
 
     public String getName() {
--- src/Menu.java
+++ src/Menu.java
@@ -1,6 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Menu {
+    private ArrayList<Item> total;
     private ArrayList<Item> breakFast;
     private ArrayList<Item> entrees;
     private ArrayList<Item> beverages;
@@ -8,6 +12,37 @@ public class Menu {
 
 
     public Menu(){
-        
+        importMenuList("src/All Items.csv");
+    }
+
+    private void importMenuList(String fileName)
+    {
+        try
+        {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = bufferedReader.readLine();
+
+            total = new ArrayList<Item>();
+
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                String[] menuFromCSV = line.split(":");
+
+                double price = Double.parseDouble(menuFromCSV[0].substring(1));
+                String name = menuFromCSV[1];
+                String description = menuFromCSV[2];
+                String itemType= menuFromCSV[3];
+
+                 Item a = new Item(price, name,description,itemType);
+                total.add(a);
+            }
+            bufferedReader.close();
+        }
+        catch(IOException exception)
+        {
+            // Print out the exception that occurred
+            System.out.println("Unable to access " + exception.getMessage());
+        }
     }
 }
