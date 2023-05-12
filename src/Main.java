d711ab31ade91a829c27b82023e7f567eb502c6f
BT_4N2_01 <mchoudhary4645@bths.edu>
2023-05-12 12:26:24 -0400

    hahahhaha

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Chick Fil A! How may i help you?");
+        System.out.println("Welcome to Chick Fil A! How may I help you?");
         System.out.println("A. Breakfast");
         System.out.println("B. Entrees ");
         System.out.println("C. Sides");
@@ -14,22 +14,22 @@ public class Main {
         String n = s.nextLine();
         if(n.toLowerCase().equals("a")){
             for(int i = 0; i < m.getBreakFast().size(); i++){
-                System.out.println(m.getBreakFast().get(i).getName() +" for "+ m.getBreakFast().get(i).getPrice());
+                System.out.println(i+": "+m.getBreakFast().get(i).getName() +" for "+ m.getBreakFast().get(i).getPrice());
             }
         }
         if(n.toLowerCase().equals("b")){
             for(int i = 0; i < m.getEntrees().size(); i++){
-                System.out.println(m.getEntrees().get(i).getName() +" for "+ m.getEntrees().get(i).getPrice());
+                System.out.println(i+": "+m.getEntrees().get(i).getName() +" for "+ m.getEntrees().get(i).getPrice());
             }
         }
         if(n.toLowerCase().equals("c")){
             for(int i = 0; i < m.getSides().size(); i++){
-                System.out.println(m.getSides().get(i).getName() +" for "+ m.getSides().get(i).getPrice());
+                System.out.println(i+": "+ m.getSides().get(i).getName() +" for "+ m.getSides().get(i).getPrice());
             }
         }
         if(n.toLowerCase().equals("d")){
             for(int i = 0; i < m.getBeverages().size(); i++){
-                System.out.println(m.getBeverages().get(i).getName() +" for "+ m.getBeverages().get(i).getPrice());
+                System.out.println(i+": "+m.getBeverages().get(i).getName() +" for "+ m.getBeverages().get(i).getPrice());
             }
         }
 
